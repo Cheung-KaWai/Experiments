31ebd442807238d567327d975b436cdae8adf6b9
Cheung-KaWai <kawaicheungdev@gmail.com>
2023-05-29 20:45:39 +0200

    Add experiment 1 wireframe animation

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,14 +1,16 @@
-import { Box, OrbitControls } from "@react-three/drei";
+import { Box, Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React from "react";
 import styled from "styled-components";
+import { Building } from "./Building";
 
 export const Scene = () => {
   return (
     <SceneContainer>
-      <Canvas>
-        <OrbitControls />
-        <Box />
+      <Canvas camera={{ position: [0, 15, 40] }}>
+        <OrbitControls autoRotate />
+        <Environment preset="city" />
+        <Building />
       </Canvas>
     </SceneContainer>
   );
